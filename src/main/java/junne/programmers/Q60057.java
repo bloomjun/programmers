16388a2946b6d7003bf59bd108bbdec3532699da
junbly <junhyeon47@gmail.com>
2020-02-27 00:23:21 +0900

    [2020카카오공채] 문자열 압축 - https://programmers.co.kr/learn/courses/30/lessons/60057

---
+++ src/main/java/junne/programmers/Q60057.java
@@ -0,0 +1,50 @@
+package junne.programmers;
+
+/**
+ * [2020카카오공채] 문자열 압축
+ * https://programmers.co.kr/learn/courses/30/lessons/60057
+ */
+public class Q60057 {
+    public int solution(String s) {
+        int answer = s.length();
+
+        int length = s.length();
+        for (int i = 1; i <= length / 2; ++i) { // 문자 자르는 단위(i) 만큼 반복
+            int pos = 0;
+            int compressionLength = s.length();
+            while (pos + i <= length) {
+                String origin = s.substring(pos, i + pos);
+                pos += i;
+                int count = 0;
+                while (pos + i <= length) {
+                    String target = s.substring(pos, pos + i);
+                    if (origin.equals(target)) {
+                        count++;
+                        pos += i;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (count > 0) { // 연속된 문자가 존재한다면
+                    compressionLength -= (i * count); // 연속된 문자 길이 제거
+
+                    // 숫자 표현하는 길이 추가
+                    if (count < 9) {
+                        compressionLength += 1;
+                    } else if (count < 99) {
+                        compressionLength += 2;
+                    } else if (count < 999) {
+                        compressionLength += 3;
+                    } else {
+                        compressionLength += 4;
+                    }
+                }
+            }
+
+            answer = Math.min(answer, compressionLength);
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/junne/programmers/Q60057Test.java
@@ -0,0 +1,44 @@
+package junne.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Q60057Test {
+    Q60057 question = new Q60057();
+
+    @Test
+    void test_1() {
+        String s = "aabbaccc";
+        int answer = question.solution(s);
+        assertEquals(7, answer);
+    }
+
+    @Test
+    void test_2() {
+        String s = "ababcdcdababcdcd";
+        int answer = question.solution(s);
+        assertEquals(9, answer);
+    }
+
+    @Test
+    void test_3() {
+        String s = "abcabcdede"	;
+        int answer = question.solution(s);
+        assertEquals(8, answer);
+    }
+
+    @Test
+    void test_4() {
+        String s = "abcabcabcabcdededededede";
+        int answer = question.solution(s);
+        assertEquals(14, answer);
+    }
+
+    @Test
+    void test_5() {
+        String s = "xababcdcdababcdcd";
+        int answer = question.solution(s);
+        assertEquals(17, answer);
+    }
+}
