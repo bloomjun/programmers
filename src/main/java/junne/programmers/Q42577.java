509b0c53c93e6f9fdbee601ac2f702f15a4d916b
junbly <junhyeon47@gmail.com>
2020-02-29 01:03:31 +0900

    전화번호 목록 - https://programmers.co.kr/learn/courses/30/lessons/42577

---
+++ src/main/java/junne/programmers/Q42577.java
@@ -0,0 +1,26 @@
+package junne.programmers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 전화번호 목록
+ * https://programmers.co.kr/learn/courses/30/lessons/42577
+ */
+public class Q42577 {
+
+    public boolean soultion(String[] phone_book) {
+        // 입력 값을 Set 으로 집어 넣는다.
+        Set<String> set = new HashSet<>(List.of(phone_book));
+
+        for (String number : phone_book) {
+            for (String prefix : set) {
+                if (number.startsWith(prefix) && !number.equals(prefix)) { // 접두어 확인
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/junne/programmers/Q42577Test.java
@@ -0,0 +1,36 @@
+package junne.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Q42577Test {
+    Q42577 question = new Q42577();
+
+    @Test
+    void test_1() {
+        String[] phoneBook = {"119", "97674223", "1195524421"};
+
+        boolean answer = question.soultion(phoneBook);
+
+        assertFalse(answer);
+    }
+
+    @Test
+    void test_2() {
+        String[] phoneBook = {"123", "456", "789"};
+
+        boolean answer = question.soultion(phoneBook);
+
+        assertTrue(answer);
+    }
+
+    @Test
+    void test_3() {
+        String[] phoneBook = {"12", "123", "1235", "567", "88"};
+
+        boolean answer = question.soultion(phoneBook);
+
+        assertFalse(answer);
+    }
+}
