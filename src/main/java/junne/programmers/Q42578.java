d36e2d469af4ffb6c94bc4b05547ee3728946cdb
junbly <junhyeon47@gmail.com>
2020-03-01 02:05:25 +0900

    위장  - https://programmers.co.kr/learn/courses/30/lessons/42578

---
+++ src/main/java/junne/programmers/Q42578.java
@@ -0,0 +1,30 @@
+package junne.programmers;
+
+import java.util.*;
+
+/**
+ * 위장
+ * https://programmers.co.kr/learn/courses/30/lessons/42578
+ */
+public class Q42578 {
+
+    public int solution(String[][] clothes) {
+        var map = new HashMap<String, List<String>>();
+        for (String[] clothe : clothes) {
+            List<String> list = map.get(clothe[1]);
+            if (list == null) {
+                list = new ArrayList<>(List.of(clothe[0]));
+                map.put(clothe[1], list);
+            } else {
+                list.add(clothe[0]);
+            }
+        }
+
+        // 종류가 A, B, C 일 때: A + B + C + AB + AC + BC + ABC = (A+1)(B+1)(C+1) - 1
+        int answer = 1;
+        for (List<String> list : map.values()) {
+            answer *= (list.size() + 1);
+        }
+        return answer - 1;
+    }
+}
---
+++ src/test/java/junne/programmers/Q42578Test.java
@@ -0,0 +1,35 @@
+package junne.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Q42578Test {
+    Q42578 question = new Q42578();
+
+    @Test
+    void test_1() {
+        String[][] clothes = {
+                {"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"},
+                {"green_turban", "headgear"}
+        };
+
+        int answer = question.solution(clothes);
+
+        assertEquals(5, answer);
+    }
+
+    @Test
+    void test_2() {
+        String[][] clothes = {
+                {"crow_mask", "face"},
+                {"blue_sunglasses", "face"},
+                {"smoky_makeup", "face"}
+        };
+
+        int answer = question.solution(clothes);
+
+        assertEquals(3, answer);
+    }
+}
